cfbe94b1a0fb54b0c361908c68e8f6d0849343fe
hsiehrobin <hsiehrobin@gmail.com>
2013-11-23 02:23:47 -0800

    fixed the send and receive for packets

--- ChatProgram/src/main/java/com/example/chatprogram/MainActivity.java
+++ ChatProgram/src/main/java/com/example/chatprogram/MainActivity.java
@@ -9,26 +9,25 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+//import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class MainActivity extends Activity
 {
-    private ListView mList;
-    private ArrayList<String> arrayList;
-    private MyCustomAdapter mAdapter;
+    //private ListView mList;
+    //private ArrayList<String> arrayList;
+    //private MyCustomAdapter mAdapter;
     private TCPClient mTcpClient;
-    public BufferedReader br;
+    //public BufferedReader br;
     EditText editText;
 
 
@@ -41,8 +40,7 @@ public class MainActivity extends Activity
 
         public void run()
         {
-            DatagramPacket packet;
-            byte[] buf = new byte[256];
+
             Log.d("SocketListner", "SocketListener Thread running");
 
             try
@@ -57,12 +55,13 @@ public class MainActivity extends Activity
                 {
                     final TextView t = (TextView)findViewById(R.id.textview);
 
+                    DatagramPacket recpacket;
+                    byte[] recbuf = new byte[256];
 
-
-                    packet = new DatagramPacket (buf, buf.length);
-                    socket.receive (packet);
+                    recpacket = new DatagramPacket (recbuf, recbuf.length);
+                    socket.receive (recpacket);
                     System.out.println ("Received packet");
-                    String s = new String (packet.getData());
+                    String s = new String (recpacket.getData());
 
                     CharSequence cs = t.getText ();
                     str = cs + "\r\n" +  s;
@@ -107,106 +106,49 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        arrayList = new ArrayList<String>();
+        //arrayList = new ArrayList<String>();
 
         editText = (EditText) findViewById(R.id.editText);
         Button send = (Button)findViewById(R.id.send_button);
-
+ /*
         //relate the listView from java to the one created in xml
         mList = (ListView)findViewById(R.id.list);
         mAdapter = new MyCustomAdapter(this, arrayList);
         mList.setAdapter(mAdapter);
-
+*/
         WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         multicastLock = wifi.createMulticastLock("multicastLock");
         multicastLock.setReferenceCounted(true);
         multicastLock.acquire();
 
 
-
-        // connect to the server
-        // new connectTask().execute("");
         send.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
-                //final EditText et = (EditText)findViewById(R.id.editText);
-                //Editable e = et.getText();
-                //String s = e.toString();
-
-                String s = editText.getText().toString();
-
-                //DatagramSocket socket;
-                new messageSender().execute(s);
-                //editText.setText("");
-
-
-                /*
-                try
+                if(editText.getText().toString() != null)
                 {
-                    DatagramSocket socket = new DatagramSocket ();
+                    String s = editText.getText().toString();
 
-                    byte[] buf = new byte[256];
-
-                    //String outputLine = s; //txArea.getText ();
-
-                    buf = s.getBytes ();
-                    InetAddress address = InetAddress.getByName (TCPClient.SERVERIP);
-                    DatagramPacket packet = new DatagramPacket (buf, buf.length, address, 4444);
-                    System.out.println ("About to send message");
-                    socket.send (packet);
-                    System.out.println ("Sent message");
-                }
-                catch (SocketException e1)
-                {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                }
-                catch (UnknownHostException e2)
-                {
-                    // TODO Auto-generated catch block
-                    e2.printStackTrace();
-                }
-                catch (IOException e3) {
-                    // TODO Auto-generated catch block
-                    e3.printStackTrace();
+                    //DatagramSocket socket;
+                    new messageSender().execute(s);
+                    editText.setText("");
                 }
 
 
-            */
             }
         });
 
+
+        // RESETS THE EDITTEXT BOX
         editText.setText("");
-
-
-
 
         Thread t = new Thread (new SocketListener ());
         t.start();
-/*
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String message = editText.getText().toString();
-
-                //add the text in the arrayList
-                arrayList.add("c: " + message);
-
-                //sends the message to the server
-                if (mTcpClient != null) {
-                    mTcpClient.sendMessage(message);
-                }
-
-                //refresh the list
-                mAdapter.notifyDataSetChanged();
-                editText.setText("");
-            }
-            */
 
         }
 
+    /*
     public class connectTask extends AsyncTask<String,String,TCPClient> {
 
         @Override
@@ -231,13 +173,20 @@ public class MainActivity extends Activity
             super.onProgressUpdate(values);
 
             //in the arrayList we add the messaged received from server
-            arrayList.add(values[0]);
+            //arrayList.add(values[0]);
+
             // notify the adapter that the data set has changed. This means that new message received
             // from server was added to the list
-            mAdapter.notifyDataSetChanged();
+            //mAdapter.notifyDataSetChanged();
         }
 
-    }
+    }*/
+
+
+    /*
+    SEND FUNCTION
+     */
+
 
     public class messageSender extends AsyncTask<String, String, String>
     {
@@ -245,24 +194,23 @@ public class MainActivity extends Activity
         {
             try
             {
-                //DatagramSocket socket = new DatagramSocket ();
 
-                byte[] buf = new byte[256];
+                byte[] buf;
+                if(editText.getText().toString() != null)
+                {
+                    String s = editText.getText().toString();
+                    buf = s.getBytes ();
+                    InetAddress address = InetAddress.getByName (TCPClient.SERVERIP);
+                    System.out.println(s);
+                    DatagramPacket packet = new DatagramPacket (buf, buf.length, address, TCPClient.SERVERPORT);
 
-                //String outputLine = s; //txArea.getText ();
-                //String s = "hello world";
-                String s = editText.getText().toString();
+                    System.out.println ("About to send message");
 
-
-                buf = s.getBytes ();
-                InetAddress address = InetAddress.getByName (TCPClient.SERVERIP);
-
-                DatagramPacket packet = new DatagramPacket (buf, buf.length, address, TCPClient.SERVERPORT);
-                System.out.println ("About to send message");
-                socket.send (packet);
-                System.out.println ("Sent message");
-                s = "";
-                buf = null;
+                    socket.send (packet);
+                    packet.setLength(0);
+                    System.out.println ("Sent message");
+                    //s = "";
+                }
 
 
             }
